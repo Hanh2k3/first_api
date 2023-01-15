02b07561420f1fd9c64aec654710ab35ab6f6ff8
Hanh <hanhlv.21it@vku.udn.vn>
2023-01-15 20:13:46 +0700

    create project

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const logger = require('morgan')
+
+
+
+
+
+const app = express()
+
+// Middleware 
+app.use(logger('dev')) // in ra số giấy của một request
+
+// Routes
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        message: 'server is ok'
+    })
+})
+
+
+
+// catch 404 errors and forward the to error (bắt lỗi)
+app.use((req, res, next) =>  {
+    const err = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
+// Error handler function
+app.use(() => {
+    const error = app.get('env') === 'development' ?err : {}
+    const status = err.status || 500
+
+    // response to client 
+    return res.status(status).json({
+        error : {
+            message: error.message
+        }
+    })
+})
+
+
+
+// Start server 
+const port = app.get('port') || 3000
+app.listen(port, () => console.log(`server is running ${port}`))
