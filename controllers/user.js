ee9689e8379f8ab0600d1ad8e29ad4da25b77652
Hanh <hanhlv.21it@vku.udn.vn>
2023-02-02 10:40:00 +0700

    relation for mongoDB

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+const Deck = require('../models/Deck')
 const User = require("../models/User")
 
 const index = async (req, res) => {
@@ -11,10 +12,43 @@ const getUser = async (req, res) => {
     return res.status(200).json(user)
 }
 
+const getUserDecks = async (req, res, next) => {
+    const { userId } = req.params
+  
+   
+   // get deck of user 
+    const listDeck = await User.findById(userId).populate('decks')
+
+   return res.status(200).json({listDeck: listDeck.decks})    
+}
+
 const createUser = async (req, res, next) => {
     const newUser = new User(req.body);
     await newUser.save();
     return res.json(newUser);
+}
+
+const createUserDeck = async (req, res, next) => {
+    const { userId } = req.params 
+
+    // create new Deck 
+    const newDeck = new Deck(req.body)
+
+    // get user from userId
+    const user = await User.findById(userId)
+
+    // add information user for deck 
+    newDeck.owner = user
+
+    // add deck_id into user 
+    const deckId =  newDeck._id.toString()
+    user.decks.push(deckId)
+
+    await user.save()
+    await newDeck.save()
+
+    return res.status(200).json({Deck: newDeck})
+
 }
 
 const replaceUser = async (req, res, next) => {
@@ -36,10 +70,16 @@ const updateUser = async (req, res, next) => {
     })
 }
 
+const test = (req, res) => {
+    console.log('test')
+}
 module.exports = {
     index,
     createUser,
+    createUserDeck,
     replaceUser, 
     updateUser,
-    getUser
+    getUser,
+    getUserDecks,
+    test
 }
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const router = require('express-promise-router')()
 
 const userController = require('../controllers/user')
 
+
 router.route('/')
     .get(userController.index)
     .post(userController.createUser)
@@ -12,5 +13,12 @@ router.route('/:userId')
     .get(userController.getUser)
     .put(userController.replaceUser)
     .patch(userController.updateUser)
+
+
+router.route('/:userId/decks')
+    .post(userController.createUserDeck)
+    .get(userController.getUserDecks)
+
+
     
 module.exports = router
